cf84105e4a952ce1ebe51300f12e6dd68b53a92c
Danielius7er <da4523899@gmail.com>
2025-11-15 20:43:56 +0100

    ADD LINKS

--- cal.js
+++ cal.js
@@ -1,11 +1,11 @@
 function clearDisplay() {
   document.getElementById('display').value = '';
 }
-
+// Adds a value (number or operator) to the display
 function addToDisplay(value) {
   document.getElementById('display').value += value;
 }
-
+// Evaluates the expression in the display and shows the result
 function calculate() {
   let displayValue = document.getElementById('display').value;
   let result = eval(displayValue);
